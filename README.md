a0b9e1a88ef82c7877a14cafade8e0dc07c74f51
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-06-12 19:52:53 +0530

    core: Update version number to v0.6 and improve print function

---
+++ README.md
@@ -0,0 +1,23 @@
+# Yearly Progress 
+This is a yearly progress building in python for terminals. 
+
+![Screenshot of Yearly Progress CLI](/docs/images/ss01.png)
+
+## Installing 
+
+1. Windows: 
+Add `yp.bat` to enviornment vriable. 
+Make sure to change path in the batch file to main.py where you have downloaded your file.
+
+```bat
+@echo off
+python "\path\to\your\main.py" %*
+```
+
+2. Linux: For linux just create an alias to the main.py in your `.bashrc`
+
+```bash
+alias yp='python3 /path/to/your/main.py'
+```
+
+Make sure your python version is 3 or greater.
--- main.py
+++ main.py
@@ -1,70 +1,22 @@
-from time import sleep
 from datetime import datetime
+from time import sleep
 import sys
-
-VERSION = "v0.5"
-
-class FontStyle:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
-def ordinal(n):
-    if 10 <= n % 100 <= 20:
-        suffix = 'th'
-    else:
-        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')
-    return str(n) + suffix
+from font_style import FontStyle
+from yp_manager import month_name, ordinal, progress, get_total_type_seconds, create_event, load_events, get_window_size
 
 
-def month_name(n):
-    return ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-            'August', 'September', 'October', 'November', 'December'][n - 1]
-
-def progress(type, start_time=None, end_time=None):
-
-    start_time = get_start_time(type, start_time)
-    end_time = get_end_time(type, end_time)
-
-    total = end_time - start_time
-    current = datetime.now() - start_time
-    return current / total
-
-def get_start_time(type, start_time=None):
-    if type == 'year':
-        return datetime(datetime.now().year, 1, 1, 0, 0, 0, 0)
-    elif type == 'month':
-        return datetime(datetime.now().year, datetime.now().month, 1, 0, 0, 0, 0)
-    elif type == 'day':
-        return datetime(datetime.now().year, datetime.now().month, datetime.now().day, 0, 0, 0, 0)
-    elif type == 'custom' and start_time is not None:
-        return start_time
+VERSION = "v0.6"
 
 
-def get_end_time(type, end_time=None):
-    if type == 'year':
-        return datetime(datetime.now().year, 12, 31, 23, 59, 59, 999999)
-    elif type == 'month':
-        return datetime(datetime.now().year, datetime.now().month, 30, 23, 59, 59, 999999)
-    elif type == 'day':
-        return datetime(datetime.now().year, datetime.now().month, datetime.now().day, 23, 59, 59, 999999)
-    
-    elif type == 'custom' and end_time is not None:
-        return end_time
 
-def get_total_type_seconds(type):
-    start_time = get_start_time(type)
-    end_time = get_end_time(type)
-    total_seconds =  (end_time - start_time).total_seconds()
+def print(*args, **kwargs):
+    global line_count
+    line_count += 1
+    for arg in args:
+        line_count += str(arg).count("\n")
+    # __builtins__.print(f"{line_count=}")
+    __builtins__.print(*args, **kwargs)
 
-    return int(round(total_seconds, 0))
 
 def print_title(title, value):
     print(f"{FontStyle.BOLD}{title}: {FontStyle.END}{value}")
@@ -81,64 +33,12 @@ def progress_bar(progress, size=50):
     print(f"{FontStyle.BLUE}[{'█' * int(progress * size)}{' ' * int(size - progress * size)}]{FontStyle.END}", f" {(progress*100):.8f}%", sep="")
 
 
-def get_window_size():
-    import os
-    col, row = os.get_terminal_size()
-    return col, row
-
-
 year = datetime.now().year
 month = month_name(datetime.now().month)
 day = ordinal(datetime.now().day)
 
 line_count = 0
 
-def print(*args, **kwargs):
-    global line_count
-    line_count += 1
-    for arg in args:
-        line_count += str(arg).count("\n")
-    # __builtins__.print(f"{line_count=}")
-    __builtins__.print(*args, **kwargs)
-
-
-def load_events():
-    import json
-    try:
-        events = []
-        with open("events.json", "r") as f:
-            events = json.load(f)
-
-        for event in events:
-            if "start_time" in event:
-                event["start_time"] = datetime.strptime(event["start_time"], "%Y-%m-%d %H:%M:%S")
-            if "end_time" in event:
-                event["end_time"] = datetime.strptime(event["end_time"], "%Y-%m-%d %H:%M:%S")
-
-        return events
-    except FileNotFoundError:
-        return []
-    
-def save_events(events):
-    import json
-    with open("events.json", "w") as f:
-        json.dump(events, f, indent=4, default=str)
-
-
-def create_event():
-    try:
-        events = load_events()
-        event = {}
-        __builtins__.print("Create an event: ")
-        event["title"] = input("title: ")
-        event["description"] = input("description: ")
-        event["start_time"] = datetime.strptime(input("Start time (YYYY-MM-DD HH:MM:SS): "), "%Y-%m-%d %H:%M:%S")
-        event["end_time"] = datetime.strptime(input("End time (YYYY-MM-DD HH:MM): "), "%Y-%m-%d %H:%M:%S")
-        events.append(event)
-        save_events(events)
-    except ValueError:
-        __builtins__.print("Invalid date format")
-        sys.exit(1)
 
 
 def main(): 
